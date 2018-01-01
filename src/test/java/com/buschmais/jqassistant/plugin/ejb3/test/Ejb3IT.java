dc0c327bf4070f7ab548f45904945cac3750d099
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2018-01-01 22:03:58 +0100

    Changed	import order to	pass Checkstyle's checks.

--- src/test/java/com/buschmais/jqassistant/plugin/ejb3/test/Ejb3IT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/ejb3/test/Ejb3IT.java
@@ -1,17 +1,23 @@
 package com.buschmais.jqassistant.plugin.ejb3.test;
 
-import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
-
 import java.io.IOException;
-
-import org.junit.Test;
 
 import com.buschmais.jqassistant.core.analysis.api.Result;
 import com.buschmais.jqassistant.core.analysis.api.rule.RuleException;
-import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.*;
+import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.MessageDrivenBean;
+import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.SingletonBean;
+import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.StatefulBean;
+import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.StatelessLocalBean;
+import com.buschmais.jqassistant.plugin.ejb3.test.set.beans.StatelessRemoteBean;
 import com.buschmais.jqassistant.plugin.java.test.AbstractJavaPluginIT;
+
+import org.junit.Test;
+
+import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for the EJB3 concepts.
